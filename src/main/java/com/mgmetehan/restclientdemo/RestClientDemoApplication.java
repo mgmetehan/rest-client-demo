60da701ec83e928ba1a297b6d3e09c9ebdb8bc5e
mgmetehan <mgmetehan@hotmail.com>
2023-12-30 15:45:21 +0300

    init project

---
+++ src/main/java/com/mgmetehan/restclientdemo/RestClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.restclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientDemoApplication.class, args);
+	}
+
+}
